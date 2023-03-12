f9adcada7aff8261b0b21e414201cdc70845f8ea
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-03-12 14:40:13 -0500

    autosave script

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
+
 function App() {
 
-  return (
-    <div>
-      Hello world!
-    </div>
+  return (  
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
   )
+
 }
 
 export default App
