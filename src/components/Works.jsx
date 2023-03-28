b00b39fc1c5cb866347ebeb2b4bdde1182eeb22f
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-28 09:21:53 -0500

    working on hero page

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -21,26 +21,38 @@ const Right = styled.div`
   flex: 1;
 `;
 
-const List = styled.ul``;
-const ListItem = styled.li``;
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem; 
+`;
+const ListItem = styled.li`
+  list-style: none;
+  font-size: 5rem;
+  font-weight: 400;
+  color: white;
+`;
+const Image = styled.img`
+`;
 
 const focusPoints = [
-  "Development",
-  "Project Management",
-  "Marketing",
-  "Design",
+  "Full Stack Development ",
+  
 ];
 function Works() {
   return (
     <Section>
       <Container>
         <Left>
+      
+        </Left>
+        <Right>    
           <List>
             {/* mapping out all focus point to the page using javascript */}
             {focusPoints.map(item => <ListItem>{item}</ListItem>)}
           </List>
-        </Left>
-        <Right></Right>
+          {/* <Image src="./img/cryptomami.png"/> */}
+        </Right>
       </Container>
     </Section>
   );
