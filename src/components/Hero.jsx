02dca0cfee03b697611f839fca5f861f37ec18b4
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-30 13:00:03 -0500

    autosave script

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "selenium-webdriver";
+// import { Button } from "selenium-webdriver"
 import styled from "styled-components";
 import Navbar from "./Navbar";
 
