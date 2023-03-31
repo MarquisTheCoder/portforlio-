1d170162c10d86872b9fbe183e816b1754c4c293
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-31 17:14:46 -0500

    adding containers for profile and contact container

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,30 @@ const Section = styled.div`
   background-color: transparent;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Container = Styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const ProfileContainer = Styled.div``;
+const ContactContainer = Styled.div``;
 
 function Contact(){
     return(
-        <Section>Contact</Section>
+        <Section>
+          <Container>
+            <ProfileContainer></ProfileContainer>
+            <ContactContainer></ContactContainer>
+          </Container>
+        </Section>
     )
 }
 
